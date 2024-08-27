9326c512bc5088e0da38f5457c46389f4a03b6a7
rafaelhenbraun <127894473+rafaelhenbraun@users.noreply.github.com>
2024-08-27 16:51:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+ 
+var recognition = new SpeechRecognition();
+
+var textbox = document.getElementById("textbox");
+
+function start()
+{
+    textbox.innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    if(Content  == "tire minha selfie")
+        {
+            console.log("tirando selfie --- ");
+            speak();
+        }
+
+    document.getElementById("textbox").innerHTML = Content;
+    
+     speak();
+}
+function speak(){
+var synth = window.speechSynthesis;
+
+speakData = "Tirando sua selfie em 5 segundos";
+
+var utterThis = new SpeechSynthesisUtterance(speakData);
+
+synth.speak(utterThis);
+Webcam.attach(camera);
+
+setTimeout(function()
+{
+    take_selfie();
+    save();
+}, 5000);
+}
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function take_selfie()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+    }
+
+function save()
+{
+link = document.getElementById("link");
+image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
+
